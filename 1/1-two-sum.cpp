18356111c9d9127c284d7df7199cfdd8b5e5721a
purple-mountain <lmwrong92@gmail.com>
2024-04-11 22:45:55 +0500

    1: Solution in cpp using hashmap

---
+++ 1/1-two-sum.cpp
@@ -0,0 +1,22 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+class Solution
+{
+  public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int diff = target - nums[i];
+            if (map.find(diff) != map.end())
+            {
+                return {map[diff], i};
+            }
+
+            map[nums[i]] = i;
+        }
+    }
+};
