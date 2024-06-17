e5b6ebbece58c3dd88f3fedbfd208b51309f7e57
purple-mountain <lmwrong92@gmail.com>
2024-06-17 09:23:34 +0000

    76: Slightly improved solution / still O(n)

--- 76/76-min-window-substring.cpp
+++ 76/76-min-window-substring.cpp
@@ -1,62 +1,62 @@
-#include <iostream>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
-
-bool valid(int f[58], int s[58])
-{
-    for (int i = 0; i < 58; i++)
-    {
-        if (s[i] != 0 && !(s[i] <= f[i]))
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
 
 class Solution
 {
   public:
     string minWindow(string s, string t)
     {
-        int s_count[58] = {};
-        int t_count[58] = {};
+        if (t.size() == 0)
+        {
+            return "";
+        }
+
+        unordered_map<char, int> s_map;
+        unordered_map<char, int> t_map;
         int ans_idx = -1;
         int max_len = s.size();
         string ans;
 
         for (auto ch : t)
         {
-            t_count[ch - 'A']++;
+            t_map[ch]++;
         }
 
-        for (int i = 0, j = 0; j < s.size();)
+        int have = 0, need = t_map.size();
+        for (int i = 0, j = i; j < s.size();)
         {
-            s_count[s[j] - 'A']++;
-
-            if (s_count[s[j] - 'A'] != t_count[s[j] - 'A'])
-            {
-                j++;
-            }
-            else if (valid(s_count, t_count))
+            if (have == need)
             {
                 int dist = j - i + 1;
-
-                if (max_len >= dist)
+                if (dist <= max_len)
                 {
                     max_len = dist;
                     ans_idx = i;
                 }
 
-                s_count[s[i] - 'A']--;
-                s_count[s[j] - 'A']--;
+                s_map[s[i]]--;
+                if (t_map[s[i]] && s_map[s[i]] < t_map[s[i]])
+                {
+                    have--;
+                    j++;
+                }
                 i++;
             }
             else
             {
-                j++;
+                s_map[s[j]]++;
+
+                if (t_map[s[j]] && s_map[s[j]] == t_map[s[j]])
+                {
+                    have++;
+                }
+
+                if (need != have)
+                {
+                    j++;
+                }
             }
         }
 
