5ed628fb08e02bb119f17ffb245357fe7ad231f6
purple-mountain <lmwrong92@gmail.com>
2024-06-19 04:00:32 +0000

    150: Solution O(n) using stack

---
+++ 150/150-eval-reverse-polish-notation.cpp
@@ -0,0 +1,48 @@
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    int evalRPN(vector<string> &tokens)
+    {
+        stack<int> st;
+
+        for (int i = 0; i < tokens.size(); i++)
+        {
+            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/")
+            {
+                st.push(stoi(tokens[i]));
+            }
+            else
+            {
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
+
+                if (tokens[i] == "+")
+                {
+                    st.push(a + b);
+                }
+                else if (tokens[i] == "-")
+                {
+                    st.push(a - b);
+                }
+                else if (tokens[i] == "*")
+                {
+                    st.push(a * b);
+                }
+                else if (tokens[i] == "/")
+                {
+                    st.push(a / b);
+                }
+            }
+        }
+
+        return st.top();
+    }
+};
