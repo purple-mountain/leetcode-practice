83427c73a10762f2ab0d568e715d20e95a22c471
purple-mountain <lmwrong92@gmail.com>
2024-07-02 18:27:05 +0000

    287: Solution O(n) using floyd's algo

---
+++ 287/287-find-the-duplicate-number.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int slow = 0;
+        int fast = 0;
+
+        while (true)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast)
+            {
+                break;
+            }
+        }
+
+        for (int i = 0; i != slow; i = nums[i], slow = nums[slow])
+            ;
+
+        return slow;
+    }
+};
